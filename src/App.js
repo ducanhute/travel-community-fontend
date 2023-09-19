c8a398346934571e9c6c59714d74af6131079ec3
ducanhute <anhhdtech@gmail.com>
2023-09-19 14:34:37 +0700

    feat: implement redux in project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
 
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import memories from "./images/memories.png";
+import useStyles from "./styles";
 const App = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>Well come to my app</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">
+                    Memories
+                </Typography>
+                <img className={classes.image} src={memories} alt="Memories" height="60" width="60"></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid justifyContent=" space-between" alignItems=" stretch">
+                        <Grid item xs={12} sm={7}>
+                            <Posts></Posts>
+                        </Grid>
+                        <Grid item xs={12} sm={7}>
+                            <Form></Form>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 };
 
